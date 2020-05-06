968acc5fa69304485dd0b4ad4ace55dbb82c6e28
teto72 <tsubasa.k.729@gmail.com>
2020-05-06 22:29:18 +0900

    style:不必要なコード削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_06_100239) do
+ActiveRecord::Schema.define(version: 2020_05_06_130239) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
     t.text "text"
     t.string "image"
     t.datetime "created_at", null: false
